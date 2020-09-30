2df576280fbc9aac98034eae928563a257d3f008
Mike Kanning <mkanning@upenn.edu>
2020-09-30 11:48:19 -0400

    attempt to simplify ill form view. remove obsolete code. i18n-ize some regularly changing text.

--- app/controllers/concerns/pre_processor.rb
+++ app/controllers/concerns/pre_processor.rb
@@ -34,61 +34,46 @@ module PreProcessor
         return { params: params }
       when 'resourcesharing'
         return {user: User.new(username)}
-      when 'ill', 'facultyexpress'
-        proxy_id = nil
-        unless params['upennproxyid'].nil? || params['upennproxyid'].empty?
-          proxy_id = params['upennproxyid']
-        end
-        user = User.new(username, proxy_id)
-        if Rails.env.in? %w[test development]
-          # populate User with whatever is needed to move things along in dev
-          # eventually, TinyTDS can be fixed or eliminated and this can be removed
-          user.data['proxied_for'] = username
-          user.data['proxied_by'] = ''
-        else
-          Illiad.getIlliadUserInfo(user, params)
-        end
-        # Show ILL "Blocked" page if user has blocked status flags
-        if user.ill_block?
-          redirect_to ill_problem_path
-          return
-        end
+    when 'ill', 'facultyexpress'
 
-        record = Illiad.getBibData(params)
-        delivery_method = determine_delivery_method(record, user, params)
-        show_addr_msg = false
-        if user.data['status'] == 'StandingFaculty'
-          Illiad.getCorrectedDeptDetails(user.data)
-          # TODO: should 'Book' be 'book'? See: https://github.com/upenn-libraries/discovery-app/commit/2cf7b58692b9fcab7562e6a985a3396f8a68ab07#diff-b1abebf4c8ccfb0aed18254a3efd8f8aR269
-          if record['requesttype'] == 'Book'
-            delivery_method = "Deliver to my department: #{user.data['delivery']}" if record['requesttype'] == 'Book'
-            show_addr_msg = true
-          end
-        end
-        # Populate author with contributor information from MARC record if author is not supplied
-        if record['author'].presence.nil? && !params['bibid'].presence.nil?
-          result = Alma::Bib.resources.almaws_v1_bibs.mms_id.get(Alma::Bib.query_merge(:mms_id => params['bibid']))
-          xml = Nokogiri::XML(result.to_xml)
-          author = xml.xpath('.//datafield[tag="700" or tag="710" or tag="711" or tag="720"][subfield/code="a"]/subfield/__content__/text()').to_a.join("; ");
-          record['author'] = author.presence || "none specified"
-        end
-        return {record: record,
-                user: user,
-                delivery_method: delivery_method,
-                show_addr_message: show_addr_msg,
-                params: params}
+      # Setup User or Proxy info
+      proxy_id = nil
+      unless params['upennproxyid'].nil? || params['upennproxyid'].empty?
+        proxy_id = params['upennproxyid']
+      end
+      user = User.new(username, proxy_id)
+      if Rails.env.in? %w[test development]
+        # populate User with whatever is needed to move things along in dev
+        # eventually, TinyTDS can be fixed or eliminated and this can be removed
+        user.data['proxied_for'] = username
+        user.data['proxied_by'] = ''
       else
-        raise ArgumentError, "PreProcessor encountered un-configured form_id: #{form_id}"
-    end
-  end
+        Illiad.getIlliadUserInfo(user, params)
+      end
 
-  def determine_delivery_method(record, user, params)
-    if params[:deliverytype] == 'bbm'
-      'Books by Mail'
-    elsif record['requesttype'] == 'Book'
-      "Pickup at #{user.data['illoffice_name']}"
+      # Show ILL "Blocked" page if user has blocked status flags
+      if user.ill_block?
+        redirect_to ill_problem_path
+        return
+      end
+
+      # Compose requested record info hash
+      record = Illiad.getBibData(params)
+
+      # Populate author with contributor information from MARC record if author is not supplied
+      if record['author'].presence.nil? && !params['bibid'].presence.nil?
+        result = Alma::Bib.resources.almaws_v1_bibs.mms_id.get(Alma::Bib.query_merge(:mms_id => params['bibid']))
+        xml = Nokogiri::XML(result.to_xml)
+        author = xml.xpath('.//datafield[tag="700" or tag="710" or tag="711" or tag="720"][subfield/code="a"]/subfield/__content__/text()').to_a.join("; ");
+        record['author'] = author.presence || "none specified"
+      end
+
+      # Return variables for form ratial rendering
+      return { record: record, user: user, params: params,
+               explicit_bbm: params[:deliverytype] == 'bbm',
+               book_request: record['requesttype']&.downcase == 'book'}
     else
-      'Web Delivery'
+      raise ArgumentError, "PreProcessor encountered un-configured form_id: #{form_id}"
     end
   end
 end
