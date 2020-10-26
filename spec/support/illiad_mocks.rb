cf4cb4671f4e5cacfc80aff57e4bc21f271735ac
Mike Kanning <mkanning@upenn.edu>
2020-10-26 11:55:42 -0400

    mock and spec for getting user info from illiad

--- spec/support/illiad_mocks.rb
+++ spec/support/illiad_mocks.rb
@@ -11,4 +11,71 @@ module IlliadApiMocks
         status: 200, body: '{"TransactionNumber": "123456"}', headers: {}
       )
   end
+
+  def stub_get_user_transaction
+    stub_request(:get, "https://illiad.library.upenn.edu/ILLiadWebPlatform/users/testuser").
+      with(
+        headers: {
+            'Accept'=>'application/json; version=1',
+            'Apikey'=>ENV['ILLIAD_API_KEY']
+        }).
+      to_return(
+        status: 200, body: '{
+          "UserName": "testuser",
+          "ExternalUserId": "testuser",
+          "LastName": "User",
+          "FirstName": "Test",
+          "SSN": "51234567",
+          "Status": "Student",
+          "EMailAddress": "testuser@upenn.edu",
+          "Phone": "",
+          "Department": "",
+          "NVTGC": "VPL",
+          "NotificationMethod": "Electronic",
+          "DeliveryMethod": "Mail to Address",
+          "LoanDeliveryMethod": "Hold for Pickup",
+          "LastChangedDate": null,
+          "AuthorizedUsers": null,
+          "Cleared": "Yes",
+          "Web": true,
+          "Address": "",
+          "Address2": null,
+          "City": null,
+          "State": null,
+          "Zip": null,
+          "Site": null,
+          "ExpirationDate": null,
+          "Number": null,
+          "UserRequestLimit": null,
+          "Organization": null,
+          "Fax": null,
+          "ShippingAcctNo": null,
+          "ArticleBillingCategory": "Exempt",
+          "LoanBillingCategory": "Exempt",
+          "Country": null,
+          "SAddress": null,
+          "SAddress2": null,
+          "SCity": null,
+          "SState": null,
+          "SZip": null,
+          "SCountry": null,
+          "RSSID": "555555555555555555555",
+          "AuthType": "Default",
+          "UserInfo1": null,
+          "UserInfo2": null,
+          "UserInfo3": null,
+          "UserInfo4": null,
+          "UserInfo5": null,
+          "MobilePhone": null
+          }', headers: {}
+        )
+  end
+
+  def stub_update_user_transaction
+
+  end
+
+  def stub_create_user_transaction
+
+  end
 end
