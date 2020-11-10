84907bce08c0b2444d21ab9e824602147204ab36
Mike Kanning <mkanning@upenn.edu>
2020-11-10 16:21:06 -0500

    add message discouraging requests for entire books/journals. move some common boolean methods into ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def article_request?
+    request_type == 'article'
+  end
+
+  def book_request?
+    request_type == 'book'
+  end
+
+  def scandelivery_request?
+    request_type == 'scandelivery'
+  end
+
+  def explicit_bbm?
+    params[:deliverytype] == 'bbm'
+  end
+
+  def request_type
+    params[:requesttype]&.downcase
+  end
 end
