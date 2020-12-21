ad8c8358f3be9d8f7f5f4d651e168ca3ebcb96fb
Mike Kanning <mkanning@upenn.edu>
2020-12-21 15:27:31 -0500

    clean up Alma gem class overrides

---
+++ config/initializers/alma_bib_item_override.rb
@@ -0,0 +1,82 @@
+# TODO: push these up to the Alma gem? Some of these are Penn-specific, others are not
+class Alma::BibItem
+  ETAS_TEMPORARY_LOCATION = 'Van Pelt - Non Circulating'
+  PHYSICAL_ITEM_DELIVERY_OPTIONS = [:pickup, :booksbymail, :scandeliver]
+  RESTRICTED_ITEM_DELIVERY_OPTIONS = [:scandeliver]
+
+  # @return [String]
+  def pid
+    item_data.dig 'pid'
+  end
+
+  # Determine, based on various response attributes, if this Item is
+  # able to be circulated.
+  # @return [TrueClass, FalseClass]
+  def checkoutable?
+    in_place? &&
+      !non_circulating? &&
+      !etas_restricted? &&
+      !not_loanable?
+  end
+
+  # Penn uses "Non-circ" in Alma
+  def non_circulating?
+    circulation_policy.include?('Non-circ')
+  end
+
+  # @return [String]
+  def user_due_date
+    item_data.dig 'due_date'
+  end
+
+  # @return [String]
+  def user_due_date_policy
+    item_data.dig 'due_date_policy'
+  end
+
+  # @return [TrueClass, FalseClass]
+  def not_loanable?
+    user_due_date_policy&.include? 'Not loanable'
+  end
+
+  # Delivery options for this Item
+  # @return [Array]
+  def delivery_options
+    if checkoutable?
+      PHYSICAL_ITEM_DELIVERY_OPTIONS
+    else
+      RESTRICTED_ITEM_DELIVERY_OPTIONS
+    end
+  end
+
+  # Is this Item restricted from circulation due to ETAS?
+  # @return [TrueClass, FalseClass]
+  def etas_restricted?
+    # is in ETAS temporary location?
+    temp_location_name == ETAS_TEMPORARY_LOCATION
+  end
+
+  # Label text for Item radio button
+  # @return [String]
+  def label_for_radio_button
+    label_info = [
+      location_name,
+      description,
+      user_due_date_policy
+    ]
+    label_info.reject(&:blank?).join(' - ')
+  end
+
+  # Hash of data used to build Item radio button client side
+  # Used by HoldingItems API controller
+  # @return [Hash]
+  def for_radio_button
+    {
+      pid: item_data['pid'],
+      label: label_for_radio_button,
+      delivery_options: delivery_options,
+      checkoutable: checkoutable?,
+      etas_restricted: etas_restricted?,
+    }
+  end
+end
