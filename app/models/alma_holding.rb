a8243e53e9be96a409e0e66448479bff4116eeb6
Mike Kanning <mkanning@upenn.edu>
2021-01-06 10:50:40 -0500

    Allow selection of Item from a holding where availability is not 'available' - Scan Deliver is always a possibility. Update availability count badge when total_item count is not returned by the holdings API.

--- app/models/alma_holding.rb
+++ app/models/alma_holding.rb
@@ -3,18 +3,23 @@ class AlmaHolding < OpenStruct
     super hash
   end
 
+  # @return [String]
   def id
     self.holding_id
   end
 
+  # @return [String]
   def label
     "#{library} -  #{location} - #{holding_info}"
   end
 
+  # @return [TrueClass, FalseClass]
   def available?
-    self.availability == 'available'
+    self.availability == 'available' ||
+      self.availability == 'check_holdings'
   end
 
+  # @return [Integer]
   def available_items
     total_items = self.total_items.to_i
     unavailable_items = self.non_available_items.to_i
